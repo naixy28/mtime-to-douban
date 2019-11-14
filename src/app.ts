88c14a0ddd91e1e8117ddcd88c8218c45d1d753c
naixy28 <yuanyang@ruguoapp.com>
2019-11-14 20:25:34 +0800

    feat: add middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import Koa from 'koa'
+import logger from 'koa-logger'
+import json from 'koa-json'
+import Router from 'koa-router'
 
 const app = new Koa()
+const router = new Router()
+
+app.use(logger())
+app.use(json())
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.use(async ctx => {
-  ctx.body = 'Hello World'
+  ctx.body = { hello: 'world' }
 })
 
 app.listen(3000, () => {
